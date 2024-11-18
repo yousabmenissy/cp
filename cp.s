5b7e5439e27acfe013cb9047228c1f517c0bd74e
yousabbbbb <yousabmenissy@gmail.com>
2024-11-18 18:19:15 +0200

    House Keeping

--- cp.s
+++ cp.s
@@ -13,56 +13,58 @@ error: .string "error: could not open file\n"
 error_len: .quad . - error
 
 .section .bss
-.lcomm buff, 1024
+.lcomm buff, 4096
 
 .section .text
 .global _start
 _start:
     movq %rsp, %rbp
+    subq $160, %rsp
     movq %rbp, %rbx
 
     cmpq $3, (%rbx)
     jne exit_usage
 
 section0:
-    addq $16, %rbx
-    open (%rbx), O_RDONLY(%rip), $0
-    pushq %rax
+    addq $16, %rbx  # argv[1]
+    open (%rbx), O_RDONLY(%rip), $0 # open file for reading
+    movq %rax, -160(%rbp)
+
     testq %rax, %rax
     js exit_error
 
 section1:
-    subq $144, %rsp
-    stat -8(%rbp), -152(%rbp)
+    stat -160(%rbp), -144(%rbp)
     testq %rax, %rax
     js exit_error
 
-    movq -104(%rbp), %rax
+    movq -96(%rbp), %rax   # rax hold the filesize
+
     cqo
-    movq $1024, %rcx
+    movq $4096, %rcx    # block size
     div %rcx
 
 section2:
-    incq %rax
+    incq %rax   # rax hold the number of writes needed
     movq %rax, %r8
 
-    addq $8, %rbx
+    addq $8, %rbx   #argv[2]
     movq O_CREAT(%rip), %rax
-    or O_WRONLY(%rip), %rax
-    open (%rbx), %rax, S_IRWXU(%rip)
-    pushq %rax
+    or   O_WRONLY(%rip), %rax
+    open (%rbx), %rax, S_IRWXU(%rip)    # create the second file with write permissions
+    movq %rax, -152(%rbp)
     testq %rax, %rax
     js exit_error
 
 .LP0:
-    read -8(%rbp), buff(%rip), $1024
-    write -160(%rbp), buff(%rip), %rax
+    read -160(%rbp), buff(%rip), $4096
+    write -152(%rbp), buff(%rip), %rax
 
     decq %r8
     cmpq $0, %r8
     jne .LP0
 
-    close -8(%rbp)
+    close -152(%rbp)
     close -160(%rbp)
 exit_success:
     exit $0
--- lib/sys_open.s
+++ lib/sys_open.s
@@ -1,28 +1,28 @@
 .section .data
 # flags
-O_RDONLY:   .quad 0     # 0b00000000
-O_WRONLY:   .quad 1     # 0b00000001
-O_RDWR:     .quad 2     # 0b00000010
-O_CREAT:    .quad 100   # 0b01100110
-O_EXCL:     .quad 200   # 0b11001000
-O_NOCTTY:   .quad 400   # 0b110010000
-O_TRUNC:    .quad 1000  # 0b1111101000
+O_RDONLY: .quad 0     # 0b00000000
+O_WRONLY: .quad 1     # 0b00000001
+O_RDWR:   .quad 2     # 0b00000010
+O_CREAT:  .quad 100   # 0b01100110
+O_EXCL:   .quad 200   # 0b11001000
+O_NOCTTY: .quad 400   # 0b110010000
+O_TRUNC:  .quad 1000  # 0b1111101000
 
 # modes
-S_IXUSR:    .quad 100   # 0b01100110
-S_IWUSR:    .quad 200   # 0b11001000
-S_IRUSR:    .quad 400   # 0b110010000
-S_IRWXU:    .quad 700   # 0b1010111100
+S_IXUSR:  .quad 100   # 0b01100110
+S_IWUSR:  .quad 200   # 0b11001000
+S_IRUSR:  .quad 400   # 0b110010000
+S_IRWXU:  .quad 700   # 0b1010111100
 
-S_IXGRP:    .quad 10    # 0b00001010
-S_IWGRG:    .quad 20    # 0b00010100
-S_IRGRG:    .quad 40    # 0b00101000
-S_IRWXG:    .quad 70    # 0b01000110
+S_IXGRP:  .quad 10    # 0b00001010
+S_IWGRG:  .quad 20    # 0b00010100
+S_IRGRG:  .quad 40    # 0b00101000
+S_IRWXG:  .quad 70    # 0b01000110
 
-S_IXOTH:    .quad 1     # 0b00000001
-S_IWOTH:    .quad 2     # 0b00000010
-S_IROTH:    .quad 4     # 0b00000100
-S_IRWXO:    .quad 7     # 0b00000111
+S_IXOTH:  .quad 1     # 0b00000001
+S_IWOTH:  .quad 2     # 0b00000010
+S_IROTH:  .quad 4     # 0b00000100
+S_IRWXO:  .quad 7     # 0b00000111
 
 .macro open file, flags, mode
     movq \file, %rdi
